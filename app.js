27527ec807ea94a402e473177980f88bd4c85d00
Luis Garzon <luisgarzon90@yahoo.com>
2018-02-01 19:24:58 -0500

    clear and re-added dynamic grid to the main app

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const resetbutton = document.querySelector('#reset');
 const boxes = document.querySelectorAll('.box');
 const gridSizeForm = document.querySelector('#grid-size');
+const main = document.querySelector('main');
 
 // eventListeners 
 
@@ -30,4 +31,24 @@ function changeGrid(e){
 e.preventDefault();
 const gridSizeValue = +gridSizeForm.querySelector('input:checked').value;
 console.log(e);
+const grid = makeGrid(gridSizeValue);
+// set the main to the grid
+main.innerHTML = ''; //this clears the main area 
+main.appendChild(grid);
+
+
 }
+function makeGrid(size){
+
+const element = document.createElement('h1');
+element.textContent = "grid!!"; 
+return element; 
+}
+//make the grid 
+  //make a div with class of row 
+    // make a grid with class md-12 inside of row
+      // made 6 divs with class of box inside of call md-12
+         // copy and pasted x6 
+           
+
+
