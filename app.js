372aa1aeb8575af9e23f38084c2cf043f4b5a300
Luis Garzon <luisgarzon90@yahoo.com>
2018-02-01 20:16:09 -0500

    made pageload grid dynamic

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
 //variables 
-
 const resetbutton = document.querySelector('#reset');
-const boxes = document.querySelectorAll('.box');
 const gridSizeForm = document.querySelector('#grid-size');
 const main = document.querySelector('main');
 
 // eventListeners 
 
-boxes.forEach(function(box, index, boxes){
-box.addEventListener('click', toggleRed);
-});
-
 resetbutton.addEventListener('click', clearGrid);
 gridSizeForm.addEventListener('submit', changeGrid);
 
+//initialized or started 
+
+changeGrid();
 
 //Functions 
 
@@ -23,32 +20,56 @@ function toggleRed(e){
 }
 
 function clearGrid(e){
+    const boxes = document.querySelectorAll('.box');
     boxes.forEach((box, index) => {
         box.classList.remove('red');
     });
 }
 function changeGrid(e){
+    if(e){
+ 
 e.preventDefault();
+    }
 const gridSizeValue = +gridSizeForm.querySelector('input:checked').value;
 console.log(e);
 const grid = makeGrid(gridSizeValue);
 // set the main to the grid
 main.innerHTML = ''; //this clears the main area 
-main.appendChild(grid);
+grid.forEach((row) => {
+    main.appendChild(row);
+});
+
 
 
 }
 function makeGrid(size){
+    const rows = [];
+    for(let i = 0; i < size; i += 1){
 
-const element = document.createElement('h1');
-element.textContent = "grid!!"; 
-return element; 
-}
+    
+
+
 //make the grid 
   //make a div with class of row 
+    const row = document.createElement('div');
+    row.classList.add('row');
+    
+
     // make a grid with class md-12 inside of row
+    const column = document.createElement('div');
       // made 6 divs with class of box inside of call md-12
+      
+    column.classList.add('col-md-12');
+    row.appendChild(column);
          // copy and pasted x6 
-           
-
-
+        for(let i = 0; i < size; i += 1){
+            const box = document.createElement('div'); 
+            box.classList.add('box');
+            column.appendChild(box);
+            box.addEventListener('click', toggleRed);
+        }
+        rows.push(row);
+    }
+        
+    return rows 
+}
